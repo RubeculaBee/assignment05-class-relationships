d3347a9083acc47f4de8c8e877064855eb984134
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-01 16:55:19 -0700

    Implemented Apple.java Class

--- src/Cohesion/Apple.java
+++ src/Cohesion/Apple.java
@@ -1,5 +1,40 @@
 package Cohesion;
+import java.awt.Color;
 
 public class Apple
 {
+    private double volume; // size of the apple in cubic centimeters
+    private Color color;   // the color of the apples skin
+
+    Apple()
+    {
+        this.volume = 100.0;
+        this.color = Color.RED;
+    }
+
+    Apple(double volume, Color color)
+    {
+        this.volume = volume;
+        this.color = color;
+    }
+
+    double getVolume()
+    {
+        return volume;
+    }
+
+    double getWeight()
+    {
+        return volume*2;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("""
+                The volume of this apple is: %.2f
+                The weight of this apple is: %.2f
+                The colour of this apple is: \033[38;2;%d;%d;%dm%s\033[0m
+                """, volume, getWeight(), color.getRed(), color.getGreen(), color.getBlue(), color.toString());
+    }
 }
