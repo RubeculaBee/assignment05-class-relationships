13bf400432a8f507ad4400a105678d8b3f58d64d
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-01 18:16:07 -0700

    Implemented main method in ConsistencyDriver.java

--- src/Consistency/ConsistencyDriver.java
+++ src/Consistency/ConsistencyDriver.java
@@ -4,6 +4,11 @@ public class ConsistencyDriver
 {
     public static void main(String[] args)
     {
+        Circle emptyCircle = new Circle();
+        Circle definedCircle = new Circle(4);
 
+        System.out.printf("No-arg   constructor circle has radius %.2f and area %.2f\n\n", emptyCircle.getRadius(), emptyCircle.getArea());
+
+        System.out.printf("Argument constructor circle has radius %.2f and area %.2f", definedCircle.getRadius(), definedCircle.getArea());
     }
 }
