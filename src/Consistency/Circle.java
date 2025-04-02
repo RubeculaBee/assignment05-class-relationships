dd091e4f932c58e736f861abad9630cf57344ffc
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-01 18:13:12 -0700

    Implemented Circle.java

--- src/Consistency/Circle.java
+++ src/Consistency/Circle.java
@@ -2,4 +2,37 @@ package Consistency;
 
 public class Circle
 {
+    private double radius; //store the radius in units
+    //private double n;    //Also stores radius, but is unhelpfully named
+
+    // no-arg constructor
+    Circle(){}
+
+    // constructor with args
+    Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    // Consistent names
+    double getRadius()
+    {
+        return radius;
+    }
+    double getArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    // Inconsistent names
+    /*
+    double retrieveRadius()
+    {
+        return radius;
+    }
+    double obtainArea()
+    {
+        return Math.PI * radius * radius;
+    }
+    */
 }
