e2b94549170bf594f991d5fb497baa3d24e3a8dc
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-01 18:26:05 -0700

    Implemented main method in EncapsulationDriver.java

--- src/Encapsulation/EncapsulationDriver.java
+++ src/Encapsulation/EncapsulationDriver.java
@@ -4,6 +4,18 @@ public class EncapsulationDriver
 {
     public static void main(String[] args)
     {
+        Example example = new Example();
 
+        System.out.println(example.getReadable());
+        //System.out.println(example.getWriteable());  <-- err
+        System.out.println(example.getFullAccess());
+
+        //example.setReadable("New Readable!");  <-- err
+        example.setWritable("New Writeable!");
+        example.setFullAccess("New Full Access!");
+
+        // See how they've changed!
+        System.out.println(example.getReadable());
+        System.out.println(example.getFullAccess());
     }
 }
