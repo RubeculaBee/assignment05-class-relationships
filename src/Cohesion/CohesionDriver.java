9a2810699da2cdddf852e6642a09156913a21d2e
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-01 17:05:59 -0700

    Implemented CohesionDriver.java

--- src/Cohesion/CohesionDriver.java
+++ src/Cohesion/CohesionDriver.java
@@ -1,5 +1,16 @@
 package Cohesion;
 
+import java.awt.Color;
+
 public class CohesionDriver
 {
+    public static void main(String[] args)
+    {
+        Apple myRedApple = new Apple();
+        Apple myGreenApple = new Apple(50, Color.green);
+
+        System.out.println("Here's an example of the no-arg Apple constructor:\n" + myRedApple);
+
+        System.out.println("Here's an example of the two-arg Apple constructor:\n" + myGreenApple);
+    }
 }
