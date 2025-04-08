55514c7018753ac0041da6105b36ac1068cd8929
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-08 15:42:58 -0700

    Defined Guest.java

--- src/Guest.java
+++ src/Guest.java
@@ -1,3 +1,29 @@
 public class Guest
 {
+    private String name;
+    private int durationOfStay;
+
+    public Guest() {}
+
+    public Guest(String name, int durationOfStay)
+    {
+        this.name = name;
+        this.durationOfStay = durationOfStay;
+    }
+
+    public void greet()
+    {
+        System.out.println("Hey there! I'm a guest here, and my name is " + name);
+    }
+
+    public void stay()
+    {
+        durationOfStay--;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("Guest name: %s\nDays left:  %d", name, durationOfStay);
+    }
 }
