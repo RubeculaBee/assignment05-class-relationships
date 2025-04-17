07eb5fe49624d802cb40d66bccf741311e4bf9e6
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-16 19:43:21 -0700

    Shape getters and setters no longer abstract

--- src/Shape.java
+++ src/Shape.java
@@ -3,6 +3,21 @@ public abstract class Shape
     private double area;
     private double perimeter;
 
-    public abstract double getArea();
-    public abstract double getPerimeter();
+    public double getArea()
+    {
+        return area;
+    }
+    public void setArea(double area)
+    {
+        this.area = area;
+    }
+
+    public double getPerimeter()
+    {
+        return perimeter;
+    }
+    public void setPerimeter(double perimeter)
+    {
+        this.perimeter = perimeter;
+    }
 }
