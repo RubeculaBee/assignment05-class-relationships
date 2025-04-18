b62324331f3e2602c2d2d54b2362edc03d8c77e8
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-17 20:17:08 -0700

    Added rectangleTest to PART_C Program

--- src/OOP_ClassRelationship_Inheritance_IS_A_RobinLane.java
+++ src/OOP_ClassRelationship_Inheritance_IS_A_RobinLane.java
@@ -2,6 +2,28 @@ public class OOP_ClassRelationship_Inheritance_IS_A_RobinLane
 {
     public static void main(String[] args)
     {
+        rectangleTest();
+    }
+
+    static void rectangleTest()
+    {
+        System.out.println("\n... Creating Rectangle (10, 25) ...");
+        Rectangle myRect = new Rectangle(10, 25);
+
+        System.out.println("\n...        toString Test        ...");
+        System.out.println(myRect);
+
+        System.out.println("\n...     setSize Test (30,6)     ...");
+        myRect.setSize(30, 6);
+        System.out.println(myRect);
+
+        System.out.println("\n...      setArea Test (100)      ...");
+        myRect.setArea(100);
+        System.out.println(myRect);
+
+        System.out.println("\n...    setPerimeter Test (60)    ...");
+        myRect.setPerimeter(60);
+        System.out.println(myRect);
 
     }
 }
