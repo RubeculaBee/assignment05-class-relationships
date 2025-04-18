c17f37e68ca2b007f4790d1c14a21865fba61ee9
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-17 20:35:02 -0700

    Added squareTest to PART_C program

--- src/OOP_ClassRelationship_Inheritance_IS_A_RobinLane.java
+++ src/OOP_ClassRelationship_Inheritance_IS_A_RobinLane.java
@@ -2,7 +2,29 @@ public class OOP_ClassRelationship_Inheritance_IS_A_RobinLane
 {
     public static void main(String[] args)
     {
-        rectangleTest();
+        //rectangleTest();
+        squareTest();
+    }
+
+    static void squareTest()
+    {
+        System.out.println("\n...   Creating Square (5)  ...");
+        Square mySquare = new Square(5);
+
+        System.out.println("\n...      toString Test     ...");
+        System.out.println(mySquare);
+
+        System.out.println("\n...    setSize Test (15)   ...");
+        mySquare.setSize(15);
+        System.out.println(mySquare);
+
+        System.out.println("\n...   setArea Test (111)   ...");
+        mySquare.setArea(111);
+        System.out.println(mySquare);
+
+        System.out.println("\n... setPerimeter Test (57) ...");
+        mySquare.setPerimeter(57);
+        System.out.println(mySquare);
     }
 
     static void rectangleTest()
