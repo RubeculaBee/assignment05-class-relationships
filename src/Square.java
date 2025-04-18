264c0fcdb111ddbf2040877b141a2a2097f84d82
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-17 20:27:20 -0700

    Implemented Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,12 @@
 public class Square extends Rectangle
 {
+    public Square(double sideLength)
+    {
+        super(sideLength, sideLength);
+    }
+
+    public void setSize(double sideLength)
+    {
+        super.setSize(sideLength, sideLength);
+    }
 }
