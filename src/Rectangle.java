8d7716fdc9117026d7f5a5800b3f6b01e19f0b07
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-17 19:33:37 -0700

    Implemented Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,48 @@
 public class Rectangle extends Shape
 {
+    private double width;
+    private double height;
+
+    public Rectangle(){}
+
+    public Rectangle(double width, double height)
+    {
+        super(width * height, 2*(width + height));
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth()
+    {
+        return width;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public void setSize(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+        super.setArea(width * height);
+        super.setPerimeter(2 * (width + height));
+    }
+
+    @Override
+    public void setArea(double area)
+    {
+        double increase = area / this.getArea();
+
+        setSize(Math.sqrt(this.width * increase), Math.sqrt(this.height * increase));
+    }
+
+    @Override
+    public void setPerimeter(double perimeter)
+    {
+        double increase = perimeter / this.getPerimeter();
+
+        setSize(this.width * increase, this.height * increase);
+    }
 }
