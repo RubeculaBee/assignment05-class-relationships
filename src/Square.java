570bb5ee85ccacb3b7644e09ecc1387a406c15ad
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-17 20:34:38 -0700

    Overrided toString in Square.java

--- src/Square.java
+++ src/Square.java
@@ -9,4 +9,10 @@ public class Square extends Rectangle
     {
         super.setSize(sideLength, sideLength);
     }
+
+    @Override
+    public String toString()
+    {
+        return String.format("This Rectangle:\n - Side Length: %.2f\n - Area: %.2f\n - Perimeter: %.2f", this.getWidth(), this.getArea(), this.getPerimeter());
+    }
 }
