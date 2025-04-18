16bb2f7e017a7d13d2bfa1d6ae03813d95a02617
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-17 21:03:29 -0700

    Implemented Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,41 @@
 public class Circle extends Shape
 {
+    private double radius;
+
+    public Circle(){}
+
+    public Circle(double radius)
+    {
+        super(Math.PI*radius*radius, 2*Math.PI*radius);
+        this.radius = radius;
+    }
+
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+        super.setArea(Math.PI*radius*radius);
+        super.setPerimeter(2*Math.PI*radius);
+    }
+
+    @Override
+    public void setArea(double area)
+    {
+        double increase = area / this.getArea();
+
+        setRadius(increase * this.radius);
+    }
+
+    @Override
+    public void setPerimeter(double perimeter)
+    {
+        double increase = perimeter / this.getPerimeter();
+
+        setRadius(increase * this.radius);
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("This Circle:\n - Radius: %.2f\n - Area: %.2f\n - Circumference: %.2f", this.radius, this.getArea(), this.getPerimeter());
+    }
 }
